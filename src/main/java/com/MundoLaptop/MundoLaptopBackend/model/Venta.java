04082d8e64273de3fa2ac2d1b994538e51e45881
Kendarg Real <estebanreal2010@hotmail.com>
2026-09-08 12:09:52 -0500

    Venta model
    # Conflicts:
    #	src/main/java/com/MundoLaptop/MundoLaptopBackend/model/OrdenDeMantenimiento.java

--- src/main/java/com/MundoLaptop/MundoLaptopBackend/model/Venta.java
+++ src/main/java/com/MundoLaptop/MundoLaptopBackend/model/Venta.java
@@ -1,6 +1,24 @@
 package com.MundoLaptop.MundoLaptopBackend.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "ventas")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Venta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fecha_venta;
+
+    private double total_venta;
 }
 
 
