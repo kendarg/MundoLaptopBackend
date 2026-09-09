9d73202f66686b8af71a3421c4c784ea06450449
Rhonald S. parra S. <stevendpasua@gmail.com>
2026-09-09 17:03:52 -0500

    cambios en usuario y factura

--- src/main/java/com/MundoLaptop/MundoLaptopBackend/model/OrdenDeMantenimiento.java
+++ src/main/java/com/MundoLaptop/MundoLaptopBackend/model/OrdenDeMantenimiento.java
@@ -1,15 +1,15 @@
 package com.MundoLaptop.MundoLaptopBackend.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "Orden_mantenimiento")
+@Getter
+@Setter
 public class OrdenDeMantenimiento {
 
     @Id
