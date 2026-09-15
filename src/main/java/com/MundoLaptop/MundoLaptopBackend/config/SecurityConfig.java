72f1242a2374bdfcfe113b1374267b2f21d4689f
Kendarg Real <estebanreal2010@hotmail.com>
2026-09-15 13:17:57 -0500

    error de conexion

--- src/main/java/com/MundoLaptop/MundoLaptopBackend/config/SecurityConfig.java
+++ src/main/java/com/MundoLaptop/MundoLaptopBackend/config/SecurityConfig.java
@@ -93,8 +93,12 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOriginPatterns(List.of(
-                "http://localhost:*",
-                "http://127.0.0.1:*"
+                "http://localhost:5500",
+                "http://127.0.0.1:5500",
+                "http://localhost:5502",
+                "http://127.0.0.1:5502",
+                "http://localhost:3000",
+                "http://127.0.0.1:8080"
         ));
 
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
